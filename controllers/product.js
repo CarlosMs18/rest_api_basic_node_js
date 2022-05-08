cfee230cc824db089d42a88dc286fad85abe0bd8
Carlos Melgarejo <carlos.melgarejo.solorzano@hotmail.com>
2022-05-07 20:39:28 -0500

    Add Delete Product

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,26 @@
 const {validationResult} = require('express-validator')
 const Product = require('../models/products')
 const User = require('../models/user')
+
+
+
+exports.getProducts = async(req,res) => {
+    const currentPage = Number(req.query.page) || 1
+    const perPage = 2
+
+  
+   
+    const [total, products] = await Promise.all([
+        Product.find().countDocuments(),
+        Product.find().skip((currentPage - 1)*perPage).limit(perPage)
+    ])
+
+    res.status(201).json({total, products})
+  
+}
+
+
+
 exports.createProduct = async(req,res, next) => {
 
     const name = req.body.name
@@ -17,7 +37,7 @@ exports.createProduct = async(req,res, next) => {
     }
 
     const user = await User.findById(req.userId)
-    
+   
     if(!user){
         const error = new Error('Ha ocurrido un problema inesperado')
         error.statusCode = 500
@@ -46,5 +66,83 @@ try {
 
 }
 
+
+
+
+exports.updatedProduct =  async(req,res, next) => {
+    const name = req.body.name
+    const price = req.body.price
+    const description = req.body.description
+    const productId = req.params.productId
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        const error = new Error('Validation Failed')
+        error.statusCode = 401,
+        error.data = errors.array()
+        next(error)
+        return
+    }
+    
+    const userOwner = await Product.findOne({owner : req.userId})
+    if(!userOwner){
+        const error = new Error('Ha ocurrido algo inesperado')
+        error.statusCode = 500,
+        next(error)
+        return
+    }
+
+    
+
+    try {
+        const product = await Product.findById(productId)
+        if(!product){
+        const error = new Error('No se encuentra dicho producto')
+        error.statusCode = 404
+        next(error)
+        return
+        }
+
+        product.name = name
+        product.price =price 
+        product.description = description
+        await product.save()
+        res.status(201).json({msg:'Product Updtaed!'})
+        
+    } catch (error) {
+        error.statusCode = 500
+        next(error)
+        return
+    }
+}
+
+
+exports.deleteProduct = async(req, res,next) => {
+    const productId = req.params.productId
+    const product = await Product.findById(productId)
+    if(!product){
+        const error = new Error('Product Not Found')
+        error.statusCode = 404
+        next(error)
+        return
+    }
+
+    const userOwner = await Product.findOne({owner : req.userId})
+    if(!userOwner){
+        const error = new Error('Ha ocurrido algo inesperado')
+        error.statusCode = 500,
+        next(error)
+        return
+    }
+
+    try {
+        await Product.findByIdAndRemove(productId)
+        res.status(200).json({message : 'Product Eliminated Satisfactoriamente!'})
+    } catch (error) {
+        error.statusCode = 500
+        next(error)
+        return
+    }
+}
 
 
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,12 @@ const productController = require('../controllers/product')
 const isAuth = require('../middlewares/is-auth')
 const Router = require('express')
 const { check } = require('express-validator')
+
 const router = Router()
+
+
+router.get('/products',productController.getProducts)
+
 
 router.post('/create-product',isAuth,[
     check('name','El nombre  debe de tener un minimo de 3 caracteres!')
@@ -13,5 +18,21 @@ router.post('/create-product',isAuth,[
      .trim()
 
 ],productController.createProduct)
+
+
+
+router.put('/product/:productId',isAuth,[
+    check('name','El nombre  debe de tener un minimo de 3 caracteres!')
+    .isLength({min : 3})
+    .trim(),
+    check('description','La descripcion debe de tener un minimo de 7 caracteres')
+    .isLength({min : 6})
+     .trim()
+],productController.updatedProduct)
+
+
+router.delete('/product/:productId',isAuth,productController.deleteProduct)
+
+
 
 module.exports = router
